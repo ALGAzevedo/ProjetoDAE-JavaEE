fd70e7e7c8e936758095556ecc7e379ec6b70def
André Azevedo <andrelgdeazevedo@gmail.com>
2021-12-30 03:13:47 +0000

    ALGA: Refactor Date to LocalDate

--- backend/src/main/java/pt/ipleiria/estg/dei/ei/dae/cardiacos/entities/TreatmentTypes/Behaviour.java
+++ backend/src/main/java/pt/ipleiria/estg/dei/ei/dae/cardiacos/entities/TreatmentTypes/Behaviour.java
@@ -5,6 +5,7 @@ import pt.ipleiria.estg.dei.ei.dae.cardiacos.entities.PRC;
 import pt.ipleiria.estg.dei.ei.dae.cardiacos.entities.TreatmentType;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 import java.util.Date;
 
 
@@ -24,7 +25,7 @@ public class Behaviour extends TreatmentType {
     public Behaviour() {
     }
 
-    public Behaviour(String name, String description, Date startDate, Date endDate, HealthcareProfessional healthCareProfessional, PRC prc) {
+    public Behaviour(String name, String description, LocalDate startDate, LocalDate endDate, HealthcareProfessional healthCareProfessional, PRC prc) {
         super(name, description, startDate, endDate, healthCareProfessional, prc);
     }
 }
